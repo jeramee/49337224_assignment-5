a58ad4dde91e8a75bf9958700723e4896496eef5
jeramee <jerameeoliver@gmail.com>
2022-10-17 19:15:26 -0500

    Commit

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,8 @@ const myPizzaOrder = ["Pepperoni", 12.99, false];
 
 // 2) Create a function named getPizzaType that will accept an array
 // as a parameter and returns the 1st element from the array.
-function getPizzaType(arr) {
+function getPizzaType(arr)
+{
     return arr[0];
 }
 
@@ -28,7 +29,8 @@ function getPizzaType(arr) {
 // The function should return the array.
 // ===========
 // Hint: find the method that will allow you to set the decimals.
-function addTax(arr) {
+function addTax(arr)
+{
     arr.splice(2, 0, Math.round(arr[1] * 0.0925 * 100) / 100);
     return arr;
 }
@@ -40,7 +42,8 @@ function addTax(arr) {
 // 4) Create a function named completeOrder that will accept an array
 // as a parameter. The function will change the value of the 4th
 // element in the array to true and return the array.
-function completeOrder(arr) {
+function completeOrder(arr)
+{
     arr[3] = true;
     return arr;
 }
@@ -53,9 +56,11 @@ function completeOrder(arr) {
 // the numbers from 1 to whatever number the parameter is. The array
 // should not include the parameter number. The function returns
 // the array.
-function allTheNumbers(num) {
+function allTheNumbers(num)
+{
     let arr = [];
-    for (let i = 1; i < num; i++) {
+    for (let i = 1; i < num; i++)
+    {
         arr.push(i);
     }
     return arr;
@@ -69,8 +74,10 @@ function allTheNumbers(num) {
 // or endNum are even, they should be included in the returned array.
 function getEvens(startNum, endNum) {
     let arr = [];
-    for (let i = startNum; i <= endNum; i++) {
-        if (i % 2 == 0) {
+    for (let i = startNum; i <= endNum; i++)
+    {
+        if (i % 2 == 0)
+        {
             arr.push(i);
         }
     }
@@ -83,7 +90,8 @@ function getEvens(startNum, endNum) {
 // as a parameter and returns the last element of the array. The
 // array can have any number of elements. The function should always
 // return the last element.
-function getLastElement(arr) {
+function getLastElement(arr)
+{
     return arr[arr.length - 1];
 }
 
@@ -92,7 +100,8 @@ function getLastElement(arr) {
 // 8) Create a function named combineArrays that accepts two arrays
 // as parameters and combines them into one array. The function
 // should return the new combined array.
-function combineArrays(arr1, arr2) {
+function combineArrays(arr1, arr2)
+{
     return arr1.concat(arr2);
 }
 
@@ -110,15 +119,18 @@ function combineArrays(arr1, arr2) {
 // into an array, use arrays methods, and turn the array back into
 // a string. This is also a challenge on freeCodeCamp. Check the
 // help forums there if you get stuck!
-function isPalindrome(str) {
+function isPalindrome(str)
+{
     let arr = str.split(" ");
     let str2 = arr.join("");
     let str3 = str2.toLowerCase();
     let str4 = str3.split("").reverse().join("");
-    if (str3 == str4) {
+    if (str3 == str4)
+    {
         return true;
     }
-    else {
+    else
+    {
         return false;
     }
 }
